3f839c84277d36b46f67ec3ae96b882b66f49381
isabel-aa <59892426+isabel-aa@users.noreply.github.com>
2023-09-21 23:56:46 -0300

    Update settings.py

--- first_proj/settings.py
+++ first_proj/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52-6o3d=bx$c&@7h6-9pktcs8nmzx#+77!7%z%i%#*ga#%*$pj'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
